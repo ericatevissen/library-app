c0cb29da25cbdf036afd958da26228a313f20365
ericaofthewired <ericatevissen@proton.me>
2023-10-03 17:36:53 +0100

    made a form to generate books

--- script.js
+++ script.js
@@ -7,5 +7,16 @@ class Book {
     }
 }
 
-let book = new Book("Lord of the rings", "tolkien", "idk", "read");
-alert(Object.values(book));
+const books = [];
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const titleInput = document.getElementById('title').value;
+    const authorInput = document.getElementById('author').value;
+    const pagesInput = document.getElementById('pages').value;
+    const readInput = document.getElementById('read').checked;
+
+    const book = new Book(titleInput, authorInput, pagesInput, readInput);
+    books.push(book);
+});
